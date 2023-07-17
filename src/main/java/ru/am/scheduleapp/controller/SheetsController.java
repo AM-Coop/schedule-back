dccb3099ac7135fbb7ec947afc37474385fd2b05
DanisValitov <fermodil@mail.ru>
2023-07-17 09:17:46 +0300

    уход от прямого взаимодействия с гугл таблицами

--- src/main/java/ru/am/scheduleapp/controller/SheetsController.java
+++ src/main/java/ru/am/scheduleapp/controller/SheetsController.java
@@ -1,25 +1,12 @@
 package ru.am.scheduleapp.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import ru.am.scheduleapp.service.SheetsService;
-
-import java.util.List;
 
 @Controller
 @AllArgsConstructor
 public class SheetsController {
-    private static final String YOUR_SPREADSHEET_ID = "12Gbgvut6OicOSLVVZYotg9wgSyz0YYqyXJSJY9ZSe2k";
-    private static final String RANGE = "Sheet1";
 
-    private final SheetsService sheetsService;
 
-    @GetMapping("/")
-    public ResponseEntity<List<List<Object>>> getDataFromSheet() {
-        return ResponseEntity.ok(
-                sheetsService.readDataFromSheet(YOUR_SPREADSHEET_ID, RANGE)
-        );
-    }
+// todo приемка файлика с расписом
 }

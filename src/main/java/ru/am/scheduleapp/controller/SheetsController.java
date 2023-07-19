4eda2efec6164edc9b50c046e3d69ed9311c80f5
DanisValitov <fermodil@mail.ru>
2023-07-19 16:29:41 +0300

    принятие таблички

--- src/main/java/ru/am/scheduleapp/controller/SheetsController.java
+++ src/main/java/ru/am/scheduleapp/controller/SheetsController.java
@@ -1,12 +1,20 @@
 package ru.am.scheduleapp.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
-@Controller
+@RestController
 @AllArgsConstructor
 public class SheetsController {
 
-
-// todo приемка файлика с расписом
+    @PostMapping("/")
+    public Mono<ResponseEntity<String>> upload(@RequestPart Mono<FilePart> file) {
+        return file.doOnNext(f -> System.out.println(f.filename()))
+                .thenReturn(ResponseEntity.ok("ok"));
+    }
 }
